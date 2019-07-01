1c1c829e8bd5e4250e6fb7b1c4f5a6318a2b1d5a
JP Meijers <gitkraken@jpmeijers.com>
2019-07-01 11:04:25 +0200

    Do not redirect slashed as that would cause TTN to do a GET to the wrong URL. Rather redirect /v2/ to /v2

--- routes.go
+++ routes.go
@@ -16,7 +16,7 @@ func Routes() *chi.Mux {
 		middleware.RealIP,
 		middleware.Logger,
 		middleware.DefaultCompress,
-		middleware.RedirectSlashes,
+		middleware.StripSlashes,
 		middleware.Recoverer,
 		chiprometheus.NewMiddleware("ttnmapper-ingress-api", 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.5, 2, 5, 10, 100, 1000, 10000),
 	)
